a2e21353f2559ba707c3216107bfc4955c49ea15
Milton <gibranrmrz@yahoo.com>
2019-01-08 13:57:45 -0500

    Initial Commit

---
+++ Partin_Ramirez_LifeQuest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Partin_Ramirez_LifeQuest
+//
+//  Created by Gibran Ramirez on 2019-01-08.
+//  Copyright © 2019 Gibran Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
