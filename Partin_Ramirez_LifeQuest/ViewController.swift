82b00e347c2a14f0330627d914bbcd542f37aa7d
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2019-01-10 13:17:10 -0500

    o

--- Partin_Ramirez_LifeQuest/ViewController.swift
+++ Partin_Ramirez_LifeQuest/ViewController.swift
@@ -6,6 +6,15 @@ class ViewController: UIViewController {
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
