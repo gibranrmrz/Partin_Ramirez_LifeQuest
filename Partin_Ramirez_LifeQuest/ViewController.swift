89a05afdeba43efe527292994becec0661be25ab
Milton <gibranrmrz@yahoo.com>
2019-01-11 12:22:01 -0500

    Fuck you github

--- Partin_Ramirez_LifeQuest/ViewController.swift
+++ Partin_Ramirez_LifeQuest/ViewController.swift
@@ -5,10 +5,8 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var viewOutlet: UIView!
     
-    func flashButton () {
-        UIButton.animate(withDuration: 1, delay: 0.0, options: [.curveLinear, .repeat, .autoreverse, .allowUserInteraction], animations: {() -> Void in
-    }
     
     
     
@@ -21,7 +19,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.viewOutlet.alpha = 0.0
+        self.viewOutlet.layer.cornerRadius = 5.0
+        if self.viewOutlet.alpha == 0.0 {
+            //Show fade in animation
+            UIView.animate(withDuration: 1.5, delay: 0.2, options: .curveEaseOut, animations: {
+                self.viewOutlet.alpha = 1.0
+            })
+        } else {
+            // hide with fade out
+            UIView.animate(withDuration: 1.5, delay: 0.2, options: .curveEaseOut, animations: {
+                self.viewOutlet.alpha = 1.0
+            })
+        }
     }
 
 
