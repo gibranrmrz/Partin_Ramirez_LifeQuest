2cdc02c497d74f5554a5948f7a61be2e089a7caf
mrCoolsville69 <partins002@tcdsb.ca>
2019-01-11 12:30:08 -0500

    a

---
+++ Partin_Ramirez_LifeQuest/GlobalFile.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalFile.swift
+//  Partin_Ramirez_LifeQuest
+//
+//  Created by Period Three on 2019-01-11.
+//  Copyright © 2019 Gibran Ramirez. All rights reserved.
+//
+
+import Foundation
+
