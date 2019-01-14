cd2ef7edb79531ca5d5d082fdca0b08648de9e7a
mrCoolsville69 <partins002@tcdsb.ca>
2019-01-14 12:04:30 -0500

    Update GlobalFile.swift

--- Partin_Ramirez_LifeQuest/GlobalFile.swift
+++ Partin_Ramirez_LifeQuest/GlobalFile.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 
-var level = defaultlevel
-var currentXP = defaultXp
 let levelUpXP = 500
 let defaultXp = 250
 let defaultlevel = 1
+
+var level = defaultlevel
+var currentXP = defaultXp
 
 func levelUp(){
     if currentXP >= levelUpXP {
