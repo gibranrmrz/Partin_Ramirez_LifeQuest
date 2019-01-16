7e0ce573d5596843e5cefd14abf46a39c1417f19
mrCoolsville69 <partins002@tcdsb.ca>
2019-01-16 11:50:58 -0500

    a

---
+++ Partin_Ramirez_LifeQuest/GoalsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GoalsViewController.swift
+//  Partin_Ramirez_LifeQuest
+//
+//  Created by Period Three on 2019-01-16.
+//  Copyright © 2019 Gibran Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class GoalsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    var goalArray: [Int] = [0, 1]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return goalArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let ourCellsTest = tableView.dequeueReusableCell(withIdentifier: "goalCell1", for: indexPath)
+        ourCellsTest.textLabel?.text = "\(goalArray[indexPath.row])"
+        return ourCellsTest
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType != UITableViewCell.AccessoryType.checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
