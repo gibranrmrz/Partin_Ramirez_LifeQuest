cc6ea53bfcdd76c1a168d3a59d9b02627d6f5049
Milton <gibranrmrz@yahoo.com>
2019-01-21 11:58:34 -0500

    c

--- Partin_Ramirez_LifeQuest/SocialLinkViewController.swift
+++ Partin_Ramirez_LifeQuest/SocialLinkViewController.swift
@@ -23,7 +23,6 @@ class SocialLinkViewController: UIViewController, UITableViewDelegate, UITableVi
     var peripheralUUIDsFound = NSMutableSet()
     
     @IBOutlet weak var socialTableView: UITableView!
-    @IBOutlet weak var doneButton: UIBarButtonItem!
     
     // Table View  Stuff
     
@@ -43,6 +42,9 @@ class SocialLinkViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         blePeripheral = peripherals[indexPath.row]
         connectToDevice()
+        if blePeripheral == peripherals[indexPath.row] {
+            currentXP += 300
+        }
     }
     
     // Core Bluetooth stuff below
@@ -67,17 +69,24 @@ class SocialLinkViewController: UIViewController, UITableViewDelegate, UITableVi
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
-            //If bluetooth is working
+            // If bluetooth is working
             startScan()
             
         } else {
-       //If bluetooth is not working
+       // If bluetooth is not working
             let alertVC = UIAlertController(title: "Bluetooth isn't Working", message: "Make sure bluetooth is on", preferredStyle: .alert)
            let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action) in alertVC.dismiss(animated: true, completion: nil)
            })
             alertVC.addAction(okAction)
             present(alertVC, animated: true, completion: nil)
         }
+    }
+    
+    
+    @IBAction func DoneTapped(_ sender: Any) {
+        centralManager?.cancelPeripheralConnection(blePeripheral!)
+        print(blePeripheral!)
+    
     }
     
     @IBAction func refreshTapped(_ sender: Any) {
