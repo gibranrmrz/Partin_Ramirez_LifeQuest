e8f32cb54ea626e4d0c52cc08433dc5b72003c4f
mrCoolsville69 <partins002@tcdsb.ca>
2019-01-22 12:29:26 -0500

    love u github <3

--- Partin_Ramirez_LifeQuest/GlobalFile.swift
+++ Partin_Ramirez_LifeQuest/GlobalFile.swift
@@ -23,15 +23,64 @@ func levelUp(gained: Int){
     }
 }
 
-struct Task {
+struct Task: Codable {
     var title: String
     var completed : Bool
 }
-struct Goal {
+struct Goal: Codable {
     var title: String
     var done: Bool
 }
-struct SocialLink {
+struct SocialLink: Codable {
     var name: String
     var met: Bool
+}
+
+let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+let archiveURL = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist")
+let archiveURL2 = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist2")
+let archiveURL3 = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist3")
+let archiveURL4 = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist4")
+let archiveURL5 = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist5")
+
+func saveData() {
+    let pListEncoder = PropertyListEncoder()
+    let encodedItem = try? pListEncoder.encode(theTaskArray)
+    try? encodedItem?.write(to: archiveURL, options: .noFileProtection)
+    
+    let newEncodedItem = try? pListEncoder.encode(theGoalArray)
+    try? newEncodedItem?.write(to: archiveURL2, options: .noFileProtection)
+    
+    let evenNewerEncodedItem = try? pListEncoder.encode(theLinkArray)
+    try? evenNewerEncodedItem?.write(to: archiveURL3, options: .noFileProtection)
+    
+    let veryNewEncodedItem = try? pListEncoder.encode(level)
+    try? veryNewEncodedItem?.write(to: archiveURL3, options: .noFileProtection)
+    
+    let extremleyNewEncodedItem = try? pListEncoder.encode(currentXP)
+    try? extremleyNewEncodedItem?.write(to: archiveURL3, options: .noFileProtection)
+}
+
+func loadData() {
+    let pListDecoder = PropertyListDecoder()
+    if let retrivedItemsData = try? Data(contentsOf: archiveURL), let decodedNotes = try?
+        pListDecoder.decode(Array<Task>.self, from: retrivedItemsData) {
+        theTaskArray = decodedNotes
+    }
+    if let retrivedItemsData = try? Data(contentsOf: archiveURL2), let decodedNotes = try?
+        pListDecoder.decode(Array<Goal>.self, from: retrivedItemsData) {
+        theGoalArray = decodedNotes
+    }
+    if let retrivedItemsData = try? Data(contentsOf: archiveURL3), let decodedNotes = try?
+        pListDecoder.decode(Array<SocialLink>.self, from: retrivedItemsData) {
+        theLinkArray = decodedNotes
+    }
+    if let retrivedItemsData = try? Data(contentsOf: archiveURL4), let decodedNotes = try?
+        pListDecoder.decode(Int.self, from: retrivedItemsData) {
+        level = decodedNotes
+    }
+    if let retrivedItemsData = try? Data(contentsOf: archiveURL5), let decodedNotes = try?
+        pListDecoder.decode(Int.self, from: retrivedItemsData) {
+        currentXP = decodedNotes
+    }
 }
